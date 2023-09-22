87d196c09ad2133d0da8e12777667852e5313e73
DarthFame <mainfame@mail.ru>
2023-09-22 16:27:10 +0300

    Add link to WOTLK ref

--- Launcher/MainWindow.xaml.cs
+++ Launcher/MainWindow.xaml.cs
@@ -851,22 +851,22 @@ namespace Launcher
                 
         private void link_main_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("http://your-link.domain");
+            Process.Start("https://darth.wowka.su/index.php?/topic/10-wrath-of-the-lich-king-335/");
         }
 
         private void link_cabinet_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("http://your-link.domain");
+            Process.Start("http://darth.wowka.su/lk/");
         }
 
         private void link_registration_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("http://your-link.domain");
+            Process.Start("http://darth.wowka.su/lk/reg.php");
         }
 
         private void link_social_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("http://your-link.domain");
+            Process.Start("https://vk.com");
         }
     }
 }
